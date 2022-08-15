dca09b90e130cf466260db4686d48460e4960a0d
Anton Cherkas <99758388+Cherkasant@users.noreply.github.com>
2022-08-15 23:04:42 +0300

    Update dateFunction.js

--- app/dateFunction.js
+++ app/dateFunction.js
@@ -1,11 +1,12 @@
 export function getDate() {
   const date = new Date(Date.now());
-  const dateResult = `${date.getHours()}:${String(date.getMinutes()).padStart(
+  const dateResult = `${date.toLocaleString('en', {
+    day: 'numeric',
+  })} ${date.toLocaleString('en', {
+    month: 'long',
+  })},${date.getHours()}:${String(date.getMinutes()).padStart(
     2,
     '0'
-  )}-${date.toLocaleString('en', { day: 'numeric' })}-${date.toLocaleString(
-    'en',
-    { month: 'long' }
-  )}`;
+  )}:${date.toLocaleString('en', { second: '2-digit' })}`;
   return dateResult;
 }
